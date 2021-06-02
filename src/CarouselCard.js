5d3c15c2481889984f8d400a7599e7435c53a88a
Reetika12 <reetika.singh05@gmail.com>
2021-06-02 14:14:48 +0530

    added timer in carousel card

--- src/CarouselCard.js
+++ src/CarouselCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import styled from "styled-components"
 import Carousel from 'react-elastic-carousel';
 import './Styles/CardHover.css'
+import { render } from '@testing-library/react';
 
 const breakPoints = [
     {width:1, itemsToShow:1},
@@ -9,13 +10,52 @@ const breakPoints = [
     {width:1200, itemsToShow:3}
  ]
 
- function CarouselCard (){
+ class CarouselCard extends Component{
+     constructor(props){
+         super(props);
+         this.state={
+            countDown:100,
+            currentTime:new Date().getTime(),
+            countDowndate: new Date("june 01, 2021 00:00:00").getTime(),
+            timeLeft:""
+         }
+     }
+     componentDidMount(){
+         let {currentTime} = this.state
+         this.interval= setInterval(()=>{
+             currentTime = new Date().getTime()
+            let distance = this.state.countDowndate - currentTime
+            this.CommonFunc(distance)
+         },1000)
+     }
+     CommonFunc=(distance)=>{
+        let days=Math.floor(distance/(1000*60*60*24));
+        let hours=Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+        let minutes=Math.floor((distance % (1000*60*60))/(1000*60));
+        let seconds=Math.floor((distance % (1000*60))/(1000));
+         this.setState({
+            timeLeft:(`${days + "d"+" "+ hours +'h'+ " "+ minutes +'m'+ " "+seconds+'s'}`)
+         })
+         if(distance<0)
+         {
+            this.setState({
+                timeLeft:"Woah You have reached the day!!!"
+            })
+         }
+
+     }
+     componentWillUnmount(){
+         clearInterval(this.interval)
+     }
+      render(){
         return (
             <>
-              <h1 style={{textAlign:"center"}}>Example to set up carousel in react</h1>
-              <div>
              
+              <h1 style={{textAlign:"center"}}>Example to set up carousel in react</h1>
+              <h2>The event is going to start in {this.state.timeLeft}</h2>
+              <div>
                   <Carousel 
+                    //  enableAutoPlay={true}
                      breakPoints={breakPoints}
                   >
                         <Item>One</Item>
@@ -30,6 +70,7 @@ const breakPoints = [
               </div>
             </>
         )
+      }
 }
 
 export default CarouselCard
